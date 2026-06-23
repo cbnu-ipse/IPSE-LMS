c11a3ea6f1df14712437ba9192699fec6e0aee5b
yhw1737 <yhw1737@naver.com>
2026-06-24 08:59:04 +0900

    fix: implement web-app-origin-association endpoint to authorize PWA subdomain scope extensions

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/schedules/add/', views.add_schedule_api, name='add_schedule_api'),
     path('api/schedules/<int:sch_id>/update/', views.update_schedule_api, name='update_schedule_api'),
     path('api/schedules/<int:sch_id>/delete/', views.delete_schedule_api, name='delete_schedule_api'),
+    path('.well-known/web-app-origin-association', views.web_app_origin_association, name='web_app_origin_association'),
 ]
