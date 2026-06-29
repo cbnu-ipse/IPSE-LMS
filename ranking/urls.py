bfec1f068d7560a3dece7a2f79105321e02a8055
yhw1737 <yhw1737@naver.com>
2026-06-29 10:22:58 +0900

    feat: add community ranking page with leaves, attendance, streak tabs

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ranking_home
+from .views import ranking_home, community_ranking
 
 app_name = "ranking"
 
 urlpatterns = [
     path("", ranking_home, name="home"),
+    path("community/", community_ranking, name="community"),
 ]
