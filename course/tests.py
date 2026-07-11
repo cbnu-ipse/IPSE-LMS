62ec2bbd466c6d7a9ad8cba22636ef5ccb7c7e3a
yhw1737 <yhw1737@naver.com>
2026-07-11 12:40:12 +0900

    feat: format auto-generated course titles as subject code plus name

--- course/tests.py
+++ course/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from mypage.models import Subject
+
 from .models import Lesson
 from .services import sync_course_from_draft
 
@@ -8,9 +10,9 @@ User = get_user_model()
 
 
 class SyncCourseFromDraftTests(TestCase):
-    def test_lesson_content_uses_content_key(self):
+    def setUp(self):
         User.objects.create_superuser(username="admin", password="pw", email="a@a.com")
-        draft = {
+        self.draft = {
             "title": "자료구조",
             "description": "스택/큐/트리 개요",
             "units": [
@@ -21,9 +23,24 @@ class SyncCourseFromDraftTests(TestCase):
             ],
         }
 
-        course, created, lesson_count = sync_course_from_draft("CS101", draft)
+    def test_lesson_content_uses_content_key(self):
+        course, created, lesson_count = sync_course_from_draft("CS101", self.draft)
 
         self.assertTrue(created)
         self.assertEqual(lesson_count, 1)
         lesson = Lesson.objects.get(course=course)
         self.assertEqual(lesson.content, "## 스택\n\n스택은 ...")
+
+    def test_title_uses_subject_code_and_name_when_code_present(self):
+        Subject.objects.create(name="자료구조", code="CS101")
+
+        course, _, _ = sync_course_from_draft("자료구조", self.draft)
+
+        self.assertEqual(course.title, "CS101(자료구조)")
+
+    def test_title_falls_back_to_subject_code_arg_when_no_code(self):
+        Subject.objects.create(name="자료구조")
+
+        course, _, _ = sync_course_from_draft("자료구조", self.draft)
+
+        self.assertEqual(course.title, "자료구조")
